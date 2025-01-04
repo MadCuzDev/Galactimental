788d2d6a20f65182dc8ea935c48596969560b6bd
MadCuzDev <devan200222@gmail.com>
2025-01-03 22:58:27 -0500

    test 3

--- main.js
+++ main.js
@@ -173,33 +173,7 @@ function calcPerTick() {
     }
 }
 
-function scriptLoader(path, callback)
-{
-    let script = document.createElement('script');
-    script.type = "text/javascript";
-    script.async = true;
-    script.src = path;
-    script.onload = function(){
-        if(typeof(callback) == "function")
-        {
-            callback();
-        }
-    }
-    try
-    {
-        let scriptOne = document.getElementsByTagName('script')[0];
-        scriptOne.parentNode.insertBefore(script, scriptOne);
-    }
-    catch(e)
-    {
-        document.getElementsByTagName("head")[0].appendChild(script);
-    }
-}
-
 function load() {
-    // Load JS files
-    scriptLoader("data.js")
-
     //idk
     window.changeTab = changeTab;
     window.purchaseUpgrade = purchaseUpgrade;
