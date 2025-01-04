f7c880c68725f0488587db1e26c1128824b7e14d
MadCuzDev <devan200222@gmail.com>
2025-01-03 22:47:42 -0500

    test migration. may not work

---
+++ data.js
@@ -0,0 +1,9 @@
+export const gameData = {
+    ore: 0,
+    mine: 0,
+    land: 0,
+    satellite: 0,
+    research: 0,
+    probe: 0,
+    colony: 0
+};
--- main.js
+++ main.js
@@ -1,12 +1,4 @@
-const gameData = {
-    ore: 0,
-    mine: 0,
-    land: 0,
-    satellite: 0,
-    research: 0,
-    probe: 0,
-    colony: 0
-};
+import { gameData } from "./data.js";
 
 const cooldowns = {
     mine: 0,
@@ -181,7 +173,38 @@ function calcPerTick() {
     }
 }
 
+function scriptLoader(path, callback)
+{
+    let script = document.createElement('script');
+    script.type = "text/javascript";
+    script.async = true;
+    script.src = path;
+    script.onload = function(){
+        if(typeof(callback) == "function")
+        {
+            callback();
+        }
+    }
+    try
+    {
+        let scriptOne = document.getElementsByTagName('script')[0];
+        scriptOne.parentNode.insertBefore(script, scriptOne);
+    }
+    catch(e)
+    {
+        document.getElementsByTagName("head")[0].appendChild(script);
+    }
+}
+
 function load() {
+    // Load JS files
+    scriptLoader("data.js")
+
+    //idk
+    window.changeTab = changeTab;
+    window.purchaseUpgrade = purchaseUpgrade;
+    window.launchShip = launchShip;
+
     // Load displays
     statDisplay = document.getElementById("stat-display");
     conquestDisplay = document.getElementById("conquest-display");
